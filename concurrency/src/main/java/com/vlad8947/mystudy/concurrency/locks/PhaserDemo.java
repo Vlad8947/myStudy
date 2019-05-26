d0c3767de2ac21cc7dd48f755211e39a0d953580
Vlad8947 <31621413+Vlad8947@users.noreply.github.com>
2019-05-26 18:27:11 +0300

    sync

---
+++ concurrency/src/main/java/com/vlad8947/mystudy/concurrency/locks/PhaserDemo.java
@@ -0,0 +1,72 @@
+package com.vlad8947.mystudy.concurrency.locks;
+
+import java.util.concurrent.Phaser;
+
+public class PhaserDemo {
+
+    public static class Program {
+
+        public static void main(String[] args) {
+
+            Phaser phaser = new Phaser(1);
+            new Thread(new PhaseThread(phaser, "PhaseThread 1")).start();
+            new Thread(new PhaseThread(phaser, "PhaseThread 2")).start();
+
+            // ждем завершения фазы 0
+            int phase = phaser.getPhase();
+            phaser.arriveAndAwaitAdvance();
+            System.out.println("Фаза " + phase + " завершена");
+
+            // ждем завершения фазы 1
+            phase = phaser.getPhase();
+            phaser.arriveAndAwaitAdvance();
+            System.out.println("Фаза " + phase + " завершена");
+
+            // ждем завершения фазы 2
+            phase = phaser.getPhase();
+            phaser.arriveAndAwaitAdvance();
+            System.out.println("Фаза " + phase + " завершена");
+
+            phaser.arriveAndDeregister();
+        }
+    }
+
+    static class PhaseThread implements Runnable {
+
+        Phaser phaser;
+        String name;
+
+        PhaseThread(Phaser p, String n){
+
+            this.phaser=p;
+            this.name=n;
+            phaser.register();
+        }
+
+        public void run(){
+
+            System.out.println(name + " выполняет фазу " + phaser.getPhase());
+            phaser.arriveAndAwaitAdvance(); // сообщаем, что первая фаза достигнута
+            try{
+                Thread.sleep(200);
+            }
+            catch(InterruptedException ex){
+                System.out.println(ex.getMessage());
+            }
+
+            System.out.println(name + " выполняет фазу " + phaser.getPhase());
+            phaser.arriveAndAwaitAdvance(); // сообщаем, что вторая фаза достигнута
+            try{
+                Thread.sleep(200);
+            }
+            catch(InterruptedException ex){
+                System.out.println(ex.getMessage());
+            }
+
+            System.out.println(name + " выполняет фазу " + phaser.getPhase());
+            phaser.arriveAndDeregister(); // сообщаем о завершении фаз и удаляем с регистрации объекты
+
+        }
+    }
+
+}
